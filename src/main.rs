d7db5e145cb89bb75ef1c115bf96487fc53c49cf
JakobSchoeberle <jrschoeberle@gmail.com>
2023-12-21 05:12:59 +0000

    cantact partially implemented

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,52 @@ extern crate cantact;
 
 use adafruit_gps::{Gps, GpsSentence};
 use adafruit_gps::NmeaOutput;
+use cantact::{Interface, Frame};
+
+fn print_frame(f: Frame) {
+    let ts = match f.timestamp {
+        Some(t) => format!("{:.6}\t", t.as_secs_f32()),
+        None => String::new(),
+    };
+
+    if f.err {
+        println!("{}  ch:{} error frame", ts, f.channel);
+    }
+
+    let mut s = format!("{}  ch:{} {:03X}", ts, f.channel, f.can_id,);
+
+    s = if f.fd {
+        format!("{}   [{:02}]  ", s, f.data_len())
+    } else {
+        format!("{}   [{:01}]  ", s, f.data_len())
+    };
+
+    for b in f.data.iter().take(f.data_len()) {
+        s = format!("{}{:02X} ", s, b);
+    }
+    println!("{}", s)
+}
+
+fn gpsupdate(gps2: &mut Gps) {
+    let values = gps2.update();
+        match values {
+            GpsSentence::InvalidSentence => println!("Invalid sentence, try again"),
+            GpsSentence::InvalidBytes => println!("Invalid bytes given, try again"),
+            GpsSentence::NoConnection => println!("No connection with gps"),
+            GpsSentence::GGA(sentence) => {
+                println!("UTC: {}\nLat:{}, Long:{}, Sats:{}, MSL Alt:{}",
+                sentence.utc, sentence.lat.unwrap_or(0.0), sentence.long.unwrap_or(0.0), sentence.satellites_used,
+                sentence.msl_alt.unwrap_or(0.0));
+            }
+            GpsSentence::GSA(sentence) => {
+                println!("PDOP:{}, VDOP:{}, HDOP:{}",
+                sentence.pdop.unwrap_or(0.0), sentence.vdop.unwrap_or(0.0), sentence.hdop.unwrap_or(0.0))
+            }
+            _ => {
+                ()
+            }
+        }
+}
 
 fn main() {
 
@@ -13,33 +59,20 @@ fn main() {
     let r = gps.pmtk_220_set_nmea_updaterate("1000");
     println!("{:?}", r);
 
-    /*
-    let can = cantact::Interface::new();
-    let mut interface = can.unwrap();
-    let output = cantact::Interface::set_monitor(&mut interface, 1, true);
-    let message = output.unwrap();
-    */
 
-    fn gpsupdate(gps2: &mut Gps) {
-        let values = gps2.update();
-            match values {
-                GpsSentence::InvalidSentence => println!("Invalid sentence, try again"),
-                GpsSentence::InvalidBytes => println!("Invalid bytes given, try again"),
-                GpsSentence::NoConnection => println!("No connection with gps"),
-                GpsSentence::GGA(sentence) => {
-                    println!("UTC: {}\nLat:{}, Long:{}, Sats:{}, MSL Alt:{}",
-                    sentence.utc, sentence.lat.unwrap_or(0.0), sentence.long.unwrap_or(0.0), sentence.satellites_used,
-                    sentence.msl_alt.unwrap_or(0.0));
-                }
-                GpsSentence::GSA(sentence) => {
-                    println!("PDOP:{}, VDOP:{}, HDOP:{}",
-                    sentence.pdop.unwrap_or(0.0), sentence.vdop.unwrap_or(0.0), sentence.hdop.unwrap_or(0.0))
-                }
-                _ => {
-                    ()
-                }
-            }
-    }
+
+
+// Open a connection to the Cantact device
+//let mut cantact = Cantact::open_default().expect("Failed to open Cantact device");
+
+
+let mut cantact_interface = Interface::new()
+.expect("failed to start interface");
+
+cantact_interface.start(move |f: Frame| {
+    print_frame(f);
+})
+.expect("failed to start start");
 
     loop {
         gpsupdate(&mut gps)
