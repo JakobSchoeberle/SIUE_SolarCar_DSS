f42d9293553efc7df749bd7ceaaa1bf132ae454d
Jakob Schoeberle <105119516+JakobSchoeberle@users.noreply.github.com>
2023-12-24 13:33:29 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import asyncio
 import websockets
+import threading
 import cv2
     
 async def Video(websocket, path):
@@ -15,7 +16,6 @@ async def Video(websocket, path):
             vid = cv2.VideoCapture('videos/video1.mp4')
         try:
             while(vid.isOpened()):
-                
                 img, frame = vid.read()
                 
                 frame = cv2.resize(frame, (640, 480))
@@ -23,21 +23,41 @@ async def Video(websocket, path):
                 man = cv2.imencode('.jpg', frame, encode_param)[1]
                 #sender(man)
                 await websocket.send(man.tobytes())
-                
-        except :
-
-            
+                await websocket.wait_closed()
+        except:
             pass
 
-async def Data(websocket,path):
+async def Data(websocket, path):
     while True:
         Value1 = 1
         await websocket.send(str(Value1))
         Value2 = 2
         await websocket.send(str(Value2))
-                
-start_video_server = websockets.serve(Video, "127.0.0.1", 9997)    
-asyncio.get_event_loop().run_until_complete(start_video_server)
-start_Data_server = websockets.serve(Data, "127.0.0.1", 9998)    
-asyncio.get_event_loop().run_until_complete(start_Data_server)
-asyncio.get_event_loop().run_forever()
+     
+def Video_Server():
+    videoloop = asyncio.new_event_loop()
+    asyncio.set_event_loop(videoloop)
+
+    start_video_server = websockets.serve(Video, "127.0.0.1", 9997)  
+    videoloop.run_until_complete(start_video_server)
+    videoloop.run_forever()
+    videoloop.close()
+
+def Data_Server():
+    dataloop = asyncio.new_event_loop()
+    asyncio.set_event_loop(dataloop)
+
+    start_Data_server = websockets.serve(Data, "127.0.0.1", 9998)    
+    dataloop.run_until_complete(start_Data_server)
+    dataloop.run_forever()
+    dataloop.close()
+
+Videothread = threading.Thread(target=Video_Server)
+Videothread.start()
+
+Datathread = threading.Thread(target=Data_Server)
+Datathread.start()
+
+print("things")
+
+#asyncio.get_event_loop().run_forever()
