ed32c2f90f75e55b37ad03976b19cdf12c453399
Jakob Schoeberle <105119516+JakobSchoeberle@users.noreply.github.com>
2023-12-24 14:18:56 -0600

    Add files via upload

---
+++ WebSockets.py
@@ -0,0 +1,51 @@
+import cv2
+import asyncio
+import websockets
+
+async def Video(websocket, path):
+
+    while True:
+
+        camera = True
+        if camera == True:
+
+            vid = cv2.VideoCapture(0)
+        else:
+            vid = cv2.VideoCapture('videos/video1.mp4')
+        try:
+            while(vid.isOpened()):
+                img, frame = vid.read()
+                
+                frame = cv2.resize(frame, (640, 480))
+                encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 65]
+                man = cv2.imencode('.jpg', frame, encode_param)[1]
+                #sender(man)
+                await websocket.send(man.tobytes())
+                await websocket.wait_closed()
+        except:
+            pass
+
+async def Data(websocket, path):
+    while True:
+        Value1 = 1
+        await websocket.send(str(Value1))
+        Value2 = 2
+        await websocket.send(str(Value2))
+     
+def Video_Server():
+    videoloop = asyncio.new_event_loop()
+    asyncio.set_event_loop(videoloop)
+
+    start_video_server = websockets.serve(Video, "127.0.0.1", 9997)  
+    videoloop.run_until_complete(start_video_server)
+    videoloop.run_forever()
+    videoloop.close()
+
+def Data_Server():
+    dataloop = asyncio.new_event_loop()
+    asyncio.set_event_loop(dataloop)
+
+    start_Data_server = websockets.serve(Data, "127.0.0.1", 9998)    
+    dataloop.run_until_complete(start_Data_server)
+    dataloop.run_forever()
+    dataloop.close()
--- main.py
+++ main.py
@@ -1,63 +1,12 @@
-
-import asyncio
-import websockets
 import threading
-import cv2
-    
-async def Video(websocket, path):
+import WebSockets as Web
+VideoEnable = False
 
-    while True:
+if (VideoEnable == True):
+    Videothread = threading.Thread(target=Web.Video_Server)
+    Videothread.start()
 
-        camera = True
-        if camera == True:
-
-            vid = cv2.VideoCapture(0)
-        else:
-            vid = cv2.VideoCapture('videos/video1.mp4')
-        try:
-            while(vid.isOpened()):
-                img, frame = vid.read()
-                
-                frame = cv2.resize(frame, (640, 480))
-                encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 65]
-                man = cv2.imencode('.jpg', frame, encode_param)[1]
-                #sender(man)
-                await websocket.send(man.tobytes())
-                await websocket.wait_closed()
-        except:
-            pass
-
-async def Data(websocket, path):
-    while True:
-        Value1 = 1
-        await websocket.send(str(Value1))
-        Value2 = 2
-        await websocket.send(str(Value2))
-     
-def Video_Server():
-    videoloop = asyncio.new_event_loop()
-    asyncio.set_event_loop(videoloop)
-
-    start_video_server = websockets.serve(Video, "127.0.0.1", 9997)  
-    videoloop.run_until_complete(start_video_server)
-    videoloop.run_forever()
-    videoloop.close()
-
-def Data_Server():
-    dataloop = asyncio.new_event_loop()
-    asyncio.set_event_loop(dataloop)
-
-    start_Data_server = websockets.serve(Data, "127.0.0.1", 9998)    
-    dataloop.run_until_complete(start_Data_server)
-    dataloop.run_forever()
-    dataloop.close()
-
-Videothread = threading.Thread(target=Video_Server)
-Videothread.start()
-
-Datathread = threading.Thread(target=Data_Server)
+Datathread = threading.Thread(target=Web.Data_Server)
 Datathread.start()
 
-print("things")
-
-#asyncio.get_event_loop().run_forever()
+# ---- Input ----
